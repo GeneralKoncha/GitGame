ef8f9b75a202011606ef2fb76f5b7e276b6fa3b8
GeneralKoncha <amir2007unikum@gmail.com>
2024-10-14 23:05:50 +0300

    4

--- GitGame/main.py
+++ GitGame/main.py
@@ -4,14 +4,12 @@ pygame.init()
 
 screen = pygame.display.set_mode((1200, 800))
 pygame.display.set_caption("GitGame")
-#icon = pygame.image.load("")
-#pygame.display.set_icon(icon)
+icon = pygame.image.load("textures/icon.png")
+pygame.display.set_icon(icon)
 
 
 run = True
 while run:
-
-    screen.fill((197, 252, 252))
 
     pygame.display.flip()
 

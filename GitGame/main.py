f0e91038124fb41c76dbc8e000a888c224c19873
HochuUgrat <nnevazna44@gmail.com>
2024-10-15 00:07:43 +0300

    11

--- GitGame/main.py
+++ GitGame/main.py
@@ -1,12 +1,17 @@
 import pygame
+import ctypes
 
 pygame.init()
 
-screen = pygame.display.set_mode((1200, 800))
+user32 = ctypes.windll.user32
+user32.SetProcessDPIAware()
+ширина = user32.GetSystemMetrics(0)
+высота = user32.GetSystemMetrics(1)
+screen = pygame.display.set_mode((ширина, высота ))
 pygame.display.set_caption("GitGame")
 icon = pygame.image.load("textures/icon.png")
 pygame.display.set_icon(icon)
-
+pygame.display.toggle_fullscreen()
 
 run = True
 while run:
